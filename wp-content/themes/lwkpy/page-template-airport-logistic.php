559b91fd88d2bae7ebd64b2a2348b957127d417f
kaiyu li <krisfk@gmail.com>
2022-09-01 15:28:54 +0800

    .

--- wp-content/themes/lwkpy/page-template-airport-logistic.php
+++ wp-content/themes/lwkpy/page-template-airport-logistic.php
@@ -80,6 +80,20 @@ get_header();
 
 </div>
 
+
+<div class="yellow-bg">
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-4">1</div>
+            <div class="col-4">2</div>
+            <div class="col-4">3</div>
+        </div>
+    </div>
+</div>
+
+
 <div>
 
 
