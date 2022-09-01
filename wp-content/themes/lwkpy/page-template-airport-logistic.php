fd8899620a0870904b391a5c9774d6f49bb99c91
kaiyu li <krisfk@gmail.com>
2022-09-02 00:41:13 +0800

    .

--- wp-content/themes/lwkpy/page-template-airport-logistic.php
+++ wp-content/themes/lwkpy/page-template-airport-logistic.php
@@ -226,20 +226,23 @@ get_header();
     </div>
 
 
-    <div class="plane-foto-selecter-div">
+    <div class="plane-foto-selecter-div-outer">
+        <div class="plane-foto-selecter-div">
 
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-        <div>7</div>
-        <div>8</div>
-        <div>9</div>
-        <div>10</div>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+            <div>7</div>
+            <div>8</div>
+            <div>9</div>
+            <div>10</div>
 
+        </div>
     </div>
+
 
 </div>
 
